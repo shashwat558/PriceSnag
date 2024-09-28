f1dc96ae45b772c18819cf4322658f8abb54fc08
shashwat558 <jainshashwat528@gmail.com>
2024-09-28 16:49:07 +0530

    Bootstraped the application

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className='w-full'>
+        <nav className='nav'>
+            <Link href={"/"} className='flex items-center gap-1'>
+             
+            </Link>
+
+
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
