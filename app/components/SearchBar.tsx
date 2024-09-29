746f80c97efed2cd8ef3e4a38f67bdaa25901c46
shashwat558 <jainshashwat528@gmail.com>
2024-09-29 20:56:19 +0530

    Started logic writing

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,17 +1,41 @@
 "use client"
 
-import React from 'react'
+import React, { FormEvent, useState } from 'react'
+
+const isValidAmazonProductURL = (url: string) => {
+    try {
+        const parsedUrl = new URL(url);
+        const hostname = parsedUrl.hostname;
+
+        if(hostname.includes('amazon.com') || 
+              hostname.includes('amazon.') || 
+              hostname.endsWith('amazon'))
+
+        
+    } catch (error) {
+        
+    }
+}
 
 const SearchBar = () => {
+    const [searchUrl, setSearchUrl] = useState("");
 
-    const handleSubmit = () => {
+
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
         
+        const isValidLink = isValidAmazonProductURL(searchUrl);
+
+        alert(isValidLink ? "Valid Link")
     }
   return (
     <form className='flex flex-wrap gap-4 mt-12'
           onSubmit={handleSubmit}
     >
      <input type="text" 
+     value={searchUrl}
+     onChange={(e) => {setSearchUrl(e.target.value)}}
      placeholder='Enter product link'
      className='searchbar-input'
      
