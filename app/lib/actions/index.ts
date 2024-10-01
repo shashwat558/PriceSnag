8a6938e126050f10da383716bf9bb37c0b5167c4
shashwat558 <jainshashwat528@gmail.com>
2024-10-01 13:14:34 +0530

    Added mongodb as a database

--- app/lib/actions/index.ts
+++ app/lib/actions/index.ts
@@ -1,5 +1,6 @@
 "use server"
 
+import { connectToDB } from "../mongoose";
 import scrapeAmazonProduct from "../scraper";
 
 
@@ -9,11 +10,18 @@ export async function scrapeAndScoreProduct(productUrl: string) {
     if(!productUrl) return ;
 
     try {
+
+        connectToDB();
         const scrapedProduct = await scrapeAmazonProduct(productUrl);
+
+        if(!scrapedProduct) return;
+        
         
     } catch (error:any) {
         console.log(error.message)
         throw new Error(`Failed to create/update product: ${error.message}`)
         
     }
+
+
 }
